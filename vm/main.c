aab82295ac2574bb1d4eaaec09b9dc6440c87efc
mlabus93 <michaellabus@Michaels-MacBook-Pro.local>
2015-05-30 15:18:26 -0400

    Initial Commit

---
+++ vm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vm
+//
+//  Created by Michael Labus on 5/30/15.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
